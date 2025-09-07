a44b2a720e25444a9a11ca0dc38b9ccd44c6571d
joalvarez <alvarez.joaquinez@gmail.com>
2025-09-06 21:54:35 -0300

    chore 🧹: change and configure annotation to scan properties package
    Signed-off-by: joalvarez <alvarez.joaquinez@gmail.com>

--- src/main/java/com/joalvarez/simpleexampleapi/SimpleExampleApiApplication.java
+++ src/main/java/com/joalvarez/simpleexampleapi/SimpleExampleApiApplication.java
@@ -1,14 +1,13 @@
 package com.joalvarez.simpleexampleapi;
 
-import com.joalvarez.simpleexampleapi.config.AppBaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
-@EnableConfigurationProperties({AppBaseProperties.class})
+@ConfigurationPropertiesScan(basePackages = {"com.joalvarez.simpleexampleapi.config"})
 public class SimpleExampleApiApplication {
 
     public static void main(String[] args) {
